26fcd012c3ec33e65e41344e7746a8d2a649e302
Eitan Mosenkis <eitan@mosenkis.net>
2018-07-25 20:41:38 +0300

    Attempt to detect and support RHEL in build.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 
+if grep -qir 'red.?hat' /etc/issue then
+    DISTRO=redhat
+elif grep -qir 'ubuntu' /etc/issue then
+    DISTRO=ubuntu
+elif grep -qir 'debian' /etc/issue then
+    DISTRO=debian
+else 
+    echo 'Failed to detect Red Hat, Debian, or Ubuntu' >&2
+    exit 1
+fi
+
 . schdl.env
 virtualenv venv
 . venv/bin/activate
-sudo apt-get install build-essential npm mongodb
-sudo service mongodb start
+if [[ ${DISTRO} == redhat ]]; then
+    curl --silent --location https://rpm.nodesource.com/setup_10.x | sudo bash -
+    sudo tee '/etc/yum.repos.d/mongodb-org-4.0.repo' > /dev/null <<EOF
+[mongodb-org-4.0]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.0/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.0.asc
+EOF
+    sudo yum -y install nodejs mongodb-org-server mongodb-org
+    sudo yum -y groupinstall 'Development Tools'
+else
+    sudo ${INSTALL_CMD} build-essential npm mongodb
+    sudo service mongodb start
+fi
 pip install -r requirements.txt
 nodeenv -p -n system
 deactivate
@@ -16,3 +41,6 @@ bower install
 cd ..
 mkdir ~/schdl-bin
 ./build.sh ~/schdl-bin
+
+# bin/addSchool.py ...
+# bin/ensureIndices.py
