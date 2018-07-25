f797ecf30ea24233a45b491cd5a54911922fbc08
Eitan Mosenkis <eitan@mosenkis.net>
2018-07-25 21:26:17 +0300

    More fixes to get setup.sh working for RHEL6

--- setup.sh
+++ setup.sh
@@ -11,12 +11,9 @@ else
     exit 1
 fi
 
-. schdl.env
-virtualenv venv
-. venv/bin/activate
 if [[ ${DISTRO} == redhat ]]; then
     curl --silent --location https://rpm.nodesource.com/setup_10.x | sudo bash -
-    sudo tee '/etc/yum.repos.d/mongodb-org-4.0.repo' > /dev/null <<EOF
+    sudo tee '/etc/yum.repos.d/mongodb-org-4.0.repo' > /dev/null <<'EOF'
 [mongodb-org-4.0]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.0/x86_64/
@@ -24,12 +21,22 @@ gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.0.asc
 EOF
-    sudo yum -y install nodejs mongodb-org-server mongodb-org
+    sudo sh -c 'wget -qO- http://people.redhat.com/bkabrda/scl_python27.repo >> /etc/yum.repos.d/scl.repo'
+    sudo yum install python27
+    . /opt/rh/python27/enable
+    sudo yum -y install nodejs mongodb-org-server mongodb-org python-virtualenv
     sudo yum -y groupinstall 'Development Tools'
+    sudo semanage port -a -t mongod_port_t -p tcp 27017
+    sudo service mongod start
 else
     sudo ${INSTALL_CMD} build-essential npm mongodb
     sudo service mongodb start
 fi
+
+. schdl.env
+virtualenv venv
+. venv/bin/activate
+
 pip install -r requirements.txt
 nodeenv -p -n system
 deactivate
